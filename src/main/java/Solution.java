abbb121bc272151055cd329bed1a6080bf175f65
alek <wait82985@gmail.com>
2023-11-07 13:29:14 +0600

    initial commit. 2 test cases

---
+++ src/main/java/Solution.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class Solution {
+    public List<List<String>> partition(String s) {
+        return null;
+    }
+}
---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,35 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SolutionTest {
+    @Test
+    public void partitionTest1() {
+        String s = "aab";
+        List<List<String>> expected = new ArrayList<>(
+                List.of(
+                        new ArrayList<>(List.of("a", "a", "b")),
+                        new ArrayList<>(List.of("aa", "b"))
+                )
+        );
+        List<List<String>> actual = new Solution().partition(s);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void partitionTest2() {
+        String s = "a";
+        List<List<String>> expected = new ArrayList<>(
+                List.of(
+                        new ArrayList<>(List.of("a"))
+
+                )
+        );
+        List<List<String>> actual = new Solution().partition(s);
+
+        Assert.assertEquals(expected, actual);
+    }
+}
